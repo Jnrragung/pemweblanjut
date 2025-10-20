e3727a5d2441e30bf803d3cef65ecc14b5a7ca08
Jnrragung <jasamapta@gmail.com>
2025-10-20 15:03:48 +0700

    Update hal 21

---
+++ database/seeders/produkTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use illuminate\Database\QueryException;
+use App\Models\produk;
+class produkTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    $brg = new produk();
+    $brg->nama_produk ='Indomie oreng';
+    $brg->kategori = 1;
+    $brg->harga_satuan = 100;
+    $brg->stok = 100;
+    $brg->satuan = 3000;
+    $brg->created_at = now();
+    $brg->updated_at =now();
+    $brg->save();
+    }
+}
